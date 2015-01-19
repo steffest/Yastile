c52623ea1d92ba9ed6aa79cab3cfbf7f41c23c5e
Steffest <dev@stef.be>
2015-01-19 16:02:07 +0100

    Added Shooter example

---
+++ examples/shooter/js/main.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded",function(){
+    // sprites from http://eab.abime.net/showthread.php?t=18807&page=3
+
+    Game.init({
+        spriteSheet: "images/xenon2_sprites1_trans.png",
+        spriteMap: "js/spritemap.js",
+        tileSize: 32,
+        viewPortWidth: 12,
+        viewPortHeight: 12,
+        scaling: SCALING.CONTAIN,
+        showFPS: true,
+        showOnScreenControls: true,
+        showScore: true,
+        showHint: true,
+        onScreenControlsImage: "../../resources/controller_4way.png",
+        closeButtonImage: "../../resources/close_button.png",
+        actionButtonImage: "../../resources/action_button.png",
+        backgroundPattern: "Black",
+        start: World.generate
+    });
+
+},false);
+
+
+
---
+++ examples/shooter/js/spritemap.js
@@ -0,0 +1,10 @@
+var spriteMap = [
+    {name: "ship", l: 66, t: 99, w:32, h:27, cX: 10, cY: 10},
+    {name: "bullet", l: 37, t: 67, w:10, h:10, cX: 10, cY: 10},
+    {name: "tile1", l: 290, t: 94, w:32, h:32, cX: 10, cY: 10},
+    {name: "enemy", l: 0, t: 492, w:15, h:15, cX: 10, cY: 10},
+    {name: "enemyrotate1", l: 17, t: 492, w:15, h:15, cX: 10, cY: 10},
+    {name: "enemyrotate2", l: 34, t: 492, w:15, h:15, cX: 10, cY: 10},
+    {name: "enemyrotate3", l: 51, t: 492, w:15, h:15, cX: 10, cY: 10},
+    {name: "explosion", l: 35, t: 182, w:26, h:28, cX: 10, cY: 10}
+];
